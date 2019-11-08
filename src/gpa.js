52ca54e507df57d2f05262d772f8fbb155a1eab8
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-08 12:23:23 -0500

    completed

--- src/gpa.js
+++ src/gpa.js
@@ -1,27 +1,38 @@
 const readlineSync = require("readline-sync");
 
 console.log("\nEnter a letter grade")
-const grade = (readlineSync.question("\n"))
+let grade = (readlineSync.question("\n"))
+let grade = grade.toUpperCase();
 
-if (grade.charAt(0) == "A" || grade.charAt(0) == "a") {
+if (grade == "F+" || grade == "F-") {
+    console.log("\nInvalid")
+} else if (grade.charAt(0) == "A") {
     let numberGrade = 4;
+    if (grade.charAt(1) === "-" && numberGrade != 0) {
+    numberGrade -= .33;
 }
-else if (grade.charAt(0) = "B" || grade.charAt(0) == "b") {
+} else if (grade.charAt(0) == "B") {
     let numberGrade = 3;
-}
-else if (grade.charAt(0) = "C" || grade.charAt(0) == "c") {
+    if (grade.charAt(1) === "+" && numberGrade != 0) {
+    numberGrade += .33;
+    } else if (grade.charAt(1) === "-" && numberGrade != 0) {
+    numberGrade -= .33;
+    }
+} else if (grade.charAt(0) == "C") {
     let numberGrade = 2;
-}
-else if (grade.charAt(0) = "D" || grade.charAt(0) == "d") {
+    if (grade.charAt(1) === "+" && numberGrade != 0) {
+    numberGrade += .33;
+    } else if (grade.charAt(1) === "-" && numberGrade != 0) {
+    numberGrade -= .33;
+    }
+} else if (grade.charAt(0) == "D") {
     let numberGrade = 1;
-}
-else if (grade.charAt(0) = "F" || grade.charAt(0) == "f") {
+    if (grade.charAt(1) === "+" && numberGrade != 0) {
+    numberGrade += .33;
+    } else if (grade.charAt(1) === "-" && numberGrade != 0) {
+    numberGrade -= .33;
+    }
+} else if (grade.charAt(0) == "F") {
     let numberGrade = 0;
 }
-if (grade.charAt(1) === "+" && numberGrade != 0) {
-    numberGrade += .33;
-} else if (grade.charAt(1) === "-" && numberGrade != 0) {
-    numberGrade -= .33;
-if (numberGrade > 4){
-  numberGrade
-}
+console.log("\nYour GPA is " + numberGrade + ".")
