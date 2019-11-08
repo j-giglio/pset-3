8d943f1896ca61ff234edfaeafbfef404e0fb711
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-08 16:11:50 -0500

    fixed ands

--- src/grades.js
+++ src/grades.js
@@ -8,13 +8,13 @@ if (Number.isNaN(grade)) {
     console.log("\nInvalid.");
 } else if (grade > 100 || grade < 0) {
     console.log("\nInvalid.");
-} else if (grade <= 100 || grade >= 90) {
+} else if (grade <= 100 && grade >= 90) {
     console.log("\nYou got an A.")
-} else if (grade <= 89 || grade >= 80) {
+} else if (grade <= 89 && grade >= 80) {
     console.log("\nYou got an B.")
-} else if (grade <= 79 || grade >= 70) {
+} else if (grade <= 79 && grade >= 70) {
     console.log("\nYou got an C.")
-} else if (grade <= 69 || grade >= 60) {
+} else if (grade <= 69 && grade >= 60) {
     console.log("\nYou got an D.")
 } else if (grade <= 59) {
     console.log("\nYou got an F.")
