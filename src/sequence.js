ec761044e60637cb173ccc8256c117cafa578ad1
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-08 12:00:05 -0500

    completed

--- src/sequence.js
+++ src/sequence.js
@@ -5,17 +5,27 @@ const inptA = Number(readlineSync.question("\n"))
 const inptB = Number(readlineSync.question(""))
 const inptC = Number(readlineSync.question(""))
 
-if (Number.isNaN(number)) {
+if (Number.isNaN(inptA)) {
     console.log("\nInvalid.");
-} else if (number > Number.MAX_SAFE_INTEGER || number < Number.MIN_SAFE_INTEGER) {
+} else if (Number.isNaN(inptB)) {
+    console.log("\nInvalid.");
+} else if (Number.isNaN(inptC)) {
+    console.log("\nInvalid.");
+} else if (inptA > Number.MAX_SAFE_INTEGER || inptA < Number.MIN_SAFE_INTEGER) {
+    console.log("\nInvalid.");
+} else if (inptB > Number.MAX_SAFE_INTEGER || inptB < Number.MIN_SAFE_INTEGER) {
+    console.log("\nInvalid.");
+} else if (inptC > Number.MAX_SAFE_INTEGER || inptC < Number.MIN_SAFE_INTEGER) {
     console.log("\nInvalid.");
 } else if (inptA == inptB && inptB == inptC) {
-  console.log("\nEqual.");
-} else if (inptA > inptB || inptB >= inptC) || (inptA >= inptB || inptB > inptC) {
-  if (inptA > inptB && inptB > inptC){
-    console.log("\nStrictly Increasing");
-  }
-  else if (inptA == inptB) || (inptB == inptC) {
-    
-  }
-}
+	console.log(“\nEqual.”);
+} else if ((inptA == inptB && inptB < inptC) || (inptA < inptB && inptB == inptC)) {
+	console.log(“\nIncreasing.”);
+} else if ((inptA == inptB && inptB > inptC) || (inptA > inptB && inptB == inptC)) {
+	console.log(“\nDecreasing.”);
+} else if (inptA < inptB && inptB < inptC) {
+	console.log(“\nStrictly Increasing.”);
+} else if (inptA > inptB && inptB > inptC) {
+	console.log(“\nStrictly Decreasing.”);
+} else {
+	console.log(“\nUnordered.”);}
