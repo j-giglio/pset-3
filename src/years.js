c8aa7c0b417e4cb5c0ce86b6fbfd3ae3e9101964
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-08 20:55:21 -0500

    fixed

--- src/years.js
+++ src/years.js
@@ -1,9 +1,9 @@
 const readlineSync = require("readline-sync");
 
 const year = Number(readlineSync.question("\nEnter a year: "));
-const testA = number / 4;
-const testB = number / 100;
-const testC = number / 400;
+const testA = year / 4;
+const testB = year / 100;
+const testC = year / 400;
 
 if (Number.isNaN(year)) {
     console.log("\nInvalid.");
@@ -11,11 +11,11 @@ if (Number.isNaN(year)) {
     console.log("\nInvalid.");
 } else if (Number.isInteger(year)) {
   if (Number.isInteger(testC)) {
-    console.log("\n." + year + "is a leap year.")
-  } else if (Number.isInteger(testA)) && !(Number.isInteger(testB)){
-    console.log("\n." + year + "is a leap year.")
-  else {
-    console.log("\n." + year + "is not a leap year.")
+    console.log("\n" + year + " is a leap year.")
+  } else if ((Number.isInteger(testA)) && !(Number.isInteger(testB))){
+    console.log("\n" + year + " is a leap year.")
+  } else {
+    console.log("\n" + year + " is not a leap year.")
   }
 } else {
   console.log("\nInvalid.");
