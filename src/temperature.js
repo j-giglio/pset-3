ad635e00e72d02e1f106cfaec6ce1709c024d9b1
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-09 23:26:55 -0500

    initial commit

---
+++ src/temperature.js
@@ -0,0 +1,37 @@
+const readlineSync = require("readline-sync");
+
+const temp = Number(readlineSync.question("\nEnter a temperature: "));
+const inptScale = (readlineSync.question("Enter a scale: "));
+const scale = inptScale.toUpperCase();
+
+if (Number.isNaN(temp)) {
+    console.log("\nInvalid.");
+} else if (temp > Number.MAX_SAFE_INTEGER || temp < Number.MIN_SAFE_INTEGER) {
+    console.log("\nInvalid.");
+} else if (scale != "F" && scale != "C" && scale != "K") {
+    console.log("\nInvalid");
+} else if (scale == "F") {
+    if (temp < 32) {
+        console.log("\nSolid.");
+    } else if (temp > 212) {
+        console.log("\nGas.");
+    } else {
+        console.log("\nLiquid.");
+    }
+} else if (scale == "C") {
+    if (temp < 0) {
+        console.log("\nSolid.");
+    } else if (temp > 100) {
+        console.log("\nGas.");
+    } else {
+        console.log("\nLiquid.");
+    }
+} else if (scale == "K") {
+    if (temp < 273.2) {
+        console.log("\nSolid.");
+    } else if (temp > 373.2) {
+        console.log("\nGas.");
+    } else {
+        console.log("\nLiquid.");
+    }
+}
