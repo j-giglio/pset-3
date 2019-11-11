9841398b4c84477909bac5a800aacd0e3c4b5186
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-10 22:12:48 -0500

    added breaks after outputs

--- src/grades.js
+++ src/grades.js
@@ -5,17 +5,17 @@ let grade = (readlineSync.question("\n"))
 grade = Math.round(grade)
 
 if (Number.isNaN(grade)) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (grade > 100 || grade < 0) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (grade <= 100 && grade >= 90) {
-    console.log("\nYou got an A.")
+    console.log("\nYou got an A.\n")
 } else if (grade <= 89 && grade >= 80) {
-    console.log("\nYou got an B.")
+    console.log("\nYou got an B.\n")
 } else if (grade <= 79 && grade >= 70) {
-    console.log("\nYou got an C.")
+    console.log("\nYou got an C.\n")
 } else if (grade <= 69 && grade >= 60) {
-    console.log("\nYou got an D.")
+    console.log("\nYou got an D.\n")
 } else if (grade <= 59) {
-    console.log("\nYou got an F.")
+    console.log("\nYou got an F.\n")
 } 
