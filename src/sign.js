06f1df632ef035c0be8dd878f8366c32023d3d9a
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-10 22:22:50 -0500

    added required breaks

--- src/sign.js
+++ src/sign.js
@@ -3,13 +3,13 @@ const readlineSync = require("readline-sync");
 const number = Number(readlineSync.question("\nEnter a number: "));
 
 if (Number.isNaN(number)) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (number > Number.MAX_SAFE_INTEGER || number < Number.MIN_SAFE_INTEGER) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (number > 0){
-    console.log("\nPositive.");
+    console.log("\nPositive.\n");
 } else if (number < 0){
-    console.log("\nNegative.");
+    console.log("\nNegative.\n");
 } else if (number == 0){
-    console.log("\nZero.");
+    console.log("\nZero.\n");
 }
