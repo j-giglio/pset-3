774ba4cf2ca115fba7098e52810b50d207dd7f3c
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-10 22:17:19 -0500

    added breaks after outputs

--- src/parity.js
+++ src/parity.js
@@ -4,15 +4,15 @@ const number = Number(readlineSync.question("\nEnter an integer: "));
 const test = number / 2;
 
 if (Number.isNaN(number)) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (number > Number.MAX_SAFE_INTEGER || number < Number.MIN_SAFE_INTEGER) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (Number.isInteger(number)) {
     if (Number.isInteger(test)) {
-    console.log("\nEven.")
+    console.log("\nEven.\n")
   } else {
-  console.log("\nOdd.")
+  console.log("\nOdd.\n")
   }
 } else {
-  console.log("\nInvalid.");
+  console.log("\nInvalid.\n");
 }
