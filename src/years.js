14035ad0b321c84baeccc9926aa1273bb31afb4e
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-10 22:27:46 -0500

    added breaks after outputs

--- src/years.js
+++ src/years.js
@@ -6,17 +6,17 @@ const testB = year / 100;
 const testC = year / 400;
 
 if (Number.isNaN(year)) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (year > Number.MAX_SAFE_INTEGER || year < 0) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (Number.isInteger(year)) {
   if (Number.isInteger(testC)) {
-    console.log("\n" + year + " is a leap year.")
+    console.log("\n" + year + " is a leap year.\n")
   } else if ((Number.isInteger(testA)) && !(Number.isInteger(testB))){
-    console.log("\n" + year + " is a leap year.")
+    console.log("\n" + year + " is a leap year.\n")
   } else {
-    console.log("\n" + year + " is not a leap year.")
+    console.log("\n" + year + " is not a leap year.\n")
   }
 } else {
-  console.log("\nInvalid.");
+  console.log("\nInvalid.\n");
 }
