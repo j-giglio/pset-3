e3c0f7bd3f72b7c1b8c08e4c9d4b722212d6a647
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-10 22:26:07 -0500

    aaded breaks

--- src/temperature.js
+++ src/temperature.js
@@ -5,33 +5,33 @@ const inptScale = (readlineSync.question("Enter a scale: "));
 const scale = inptScale.toUpperCase();
 
 if (Number.isNaN(temp)) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (temp > Number.MAX_SAFE_INTEGER || temp < Number.MIN_SAFE_INTEGER) {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 } else if (scale == "F") {
     if (temp < 32) {
-        console.log("\nSolid.");
+        console.log("\nSolid.\n");
     } else if (temp > 212) {
-        console.log("\nGas.");
+        console.log("\nGas.\n");
     } else {
-        console.log("\nLiquid.");
+        console.log("\nLiquid.\n");
     }
 } else if (scale == "C") {
     if (temp < 0) {
-        console.log("\nSolid.");
+        console.log("\nSolid.\n");
     } else if (temp > 100) {
-        console.log("\nGas.");
+        console.log("\nGas.\n");
     } else {
-        console.log("\nLiquid.");
+        console.log("\nLiquid.\n");
     }
 } else if (scale == "K") {
     if (temp < 273.2) {
-        console.log("\nSolid.");
+        console.log("\nSolid.\n");
     } else if (temp > 373.2) {
-        console.log("\nGas.");
+        console.log("\nGas.\n");
     } else {
-        console.log("\nLiquid.");
+        console.log("\nLiquid.\n");
     }
 } else {
-    console.log("\nInvalid.");
+    console.log("\nInvalid.\n");
 }
